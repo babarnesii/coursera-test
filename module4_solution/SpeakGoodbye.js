28d91859d9f866abb330a504cc54113f11417ac7
Ben Barnes <b.a.barnesii@mgmail.com>
2021-02-22 20:21:48 -0500

    homework

--- module4_solution/SpeakGoodbye.js
+++ module4_solution/SpeakGoodbye.js
@@ -1,1 +1,9 @@
+(function (window) {
+var byeSpeaker = {};
+	function speak(name) {
+  		console.log(speakWord + " " + name);
+	}
+var speakWord = "Good Bye";
 
+	window.byeSpeaker = byeSpeaker;
+}) (window);
--- module4_solution/SpeakHello.js
+++ module4_solution/SpeakHello.js
@@ -1,1 +1,9 @@
+(function (window) {
+var helloSpeaker = {};
+	function speak (name) {
+		console.log(speakWord + " " + name);
+	}
+var speakWord = "Hello";
 
+	window.helloSpeaker = helloSpeaker;
+}) (window);
